d2178a91a5a0cfd66342ce6c3282488ef261f8b0
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-05-18 12:04:58 +0700

    update views and templates, add some html code

--- CTF_App/urls.py
+++ CTF_App/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
 from . import views
 
-app_name = "CTF_App"
+app_name = 'CTF_App'
+
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="article_detail"),
+    # URL pattern cho trang chủ
+    path('', views.IndexView.as_view(), name='index'),
+
+    # URL pattern cho trang chi tiết bài viết
+    path('<uuid:pk>/', views.DetailView.as_view(), name='article_detail'),
 ]
--- CTF_App/views.py
+++ CTF_App/views.py
@@ -1,8 +1,7 @@
 from django.db.models import F
 from django.views import generic
-
+from django.shortcuts import get_object_or_404
 from .models import Articles
-
 
 
 class IndexView(generic.ListView):
@@ -13,5 +12,12 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Articles.objects.order_by(F("date").desc())
 
+
 class DetailView(generic.DetailView):
     template_name = "CTF_App/article_detail.html"
+    model = Articles
+    context_object_name = "article"
+
+    def get_object(self):
+        # Lấy đối tượng bài viết dựa trên id của nó
+        return get_object_or_404(Articles, id=self.kwargs['pk'])
