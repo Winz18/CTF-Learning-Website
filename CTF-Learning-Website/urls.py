851c47a906bece516f997991a272fe40289f06c6
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-05-18 10:02:12 +0700

    optimize

--- CTF-Learning-Website/urls.py
+++ CTF-Learning-Website/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for CTF-Learning-Website project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views. home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
-"""
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -22,5 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("debug/", include("debug_toolbar.urls")),
-    path("CTF/", include("CTF_App.urls")),
+    path("", include("CTF_App.urls")),
 ]
