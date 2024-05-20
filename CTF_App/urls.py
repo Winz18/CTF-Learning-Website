af943d8d718d704eb800209991605bb188a9c35f
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-05-20 15:06:15 +0700

    add login, logout, signup and scoreboard

--- CTF_App/urls.py
+++ CTF_App/urls.py
@@ -8,5 +8,17 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 
     # URL pattern cho trang chi tiết bài viết
-    path('<uuid:pk>/', views.DetailView.as_view(), name='article_detail'),
+    path('articles/<uuid:pk>/', views.DetailView.as_view(), name='article_detail'),
+
+    # URL pattern cho trang xếp hạng
+    path('scoreboard/', views.ScoreboardView.as_view(), name='scoreboard'),
+
+    # URL pattern cho trang đăng nhập
+    path('login/', views.user_login, name='login'),
+
+    # URL pattern cho trang đăng xuất
+    path('logout/', views.user_logout, name='logout'),
+
+    # URL pattern cho trang đăng ký
+    path('signup/', views.user_signup, name='signup'),
 ]
--- CTF_App/views.py
+++ CTF_App/views.py
@@ -1,7 +1,12 @@
 from django.db.models import F
 from django.views import generic
 from django.shortcuts import get_object_or_404
-from .models import Articles
+from .models import Articles, CustomUser
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
 
 
 class IndexView(generic.ListView):
@@ -21,3 +26,61 @@ class DetailView(generic.DetailView):
     def get_object(self):
         # Lấy đối tượng bài viết dựa trên id của nó
         return get_object_or_404(Articles, id=self.kwargs['pk'])
+
+
+class ScoreboardView(generic.ListView):
+    template_name = 'CTF_App/scoreboard.html'
+    context_object_name = 'users'
+    model = User
+
+    def get_queryset(self):
+        return User.objects.annotate(score=F('customuser__score')).select_related('customuser').order_by(
+            F('customuser__score').desc(), 'date_joined')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('CTF_App:index')
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, "CTF_App/index.html", {'error_message': 'Invalid login'})
+    else:
+        return render(request, 'CTF_App/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    # Redirect to a success page.
+    return redirect('CTF_App:index')
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        # Lấy dữ liệu từ form đăng ký
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        # Tạo một user mới
+        user = User.objects.create_user(username=username, password=password, email=email)
+
+        # Lưu user mới vào cơ sở dữ liệu
+        user.save()
+
+        # Đăng nhập người dùng sau khi đăng ký (tuỳ chọn)
+        # authenticate và login giúp đăng nhập người dùng sau khi họ đăng ký
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+        # Chuyển hướng người dùng đến trang chủ hoặc trang sau khi đăng ký thành công
+        return redirect('CTF_App:index')
+
+    # Nếu không phải là phương thức POST, render template cho trang đăng ký
+    return render(request, 'CTF_App/signup.html')
