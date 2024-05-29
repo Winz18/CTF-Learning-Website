f4ee8fece6bf734b3f8664ada63428ce699a4f83
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-05-29 21:24:05 +0700

    Remove api of mobile platform - 2nd

--- CTF_App/apps.py
+++ CTF_App/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CtfAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'CTF_App'
-
-    def ready(self):
-        import CTF_App.signals
--- CTF_App/views.py
+++ CTF_App/views.py
@@ -26,9 +26,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import Articles, Sections, Test, QuestionInTest, Question, Answer, CustomUser, Comment
-from .serializers import ArticleSerializer, SectionSerializer, CommentSerializer, TestSerializer, QuestionSerializer, \
-    QuestionInTestSerializer, AnswerSerializer, CustomUserSerializer
+from .models import *
 
 
 class CommentForm(forms.ModelForm):
